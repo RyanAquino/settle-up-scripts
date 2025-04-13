c443c4203fbab6b9e2d76e1e1822d3f7a28901cc
RyanAquino <ryan.aquino7110@gmail.com>
2025-04-13 20:13:50 +0800

    refactor; add formatter

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 from loguru import logger
+from requests import Session
 
 logger.info("Loading envs... ", load_dotenv(verbose=True))
 
@@ -20,32 +21,92 @@ RAW_PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 PRIVATE_KEY = RAW_PRIVATE_KEY.replace("\\n", "\n")
 OFFSET = 2
 
-scopes = [
-    "https://www.googleapis.com/auth/spreadsheets"
-]
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 
 dt = datetime.now(tz=timezone.utc)
 curr_year = dt.year
 curr_month_num = dt.month
 month_name = calendar.month_name[curr_month_num - OFFSET]
 
+
 def init_gspread():
     credentials = {
-      "type": "service_account",
-      "project_id": "scripts-interval",
-      "private_key_id": "63c81a1a9c37e33774bcb35e2506d49bea8dc66b",
-      "private_key": PRIVATE_KEY,
-      "client_email": "scripts-interval-sa@scripts-interval.iam.gserviceaccount.com",
-      "client_id": "115821091947389819402",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/scripts-interval-sa%40scripts-interval.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
+        "type": "service_account",
+        "project_id": "scripts-interval",
+        "private_key_id": "63c81a1a9c37e33774bcb35e2506d49bea8dc66b",
+        "private_key": PRIVATE_KEY,
+        "client_email": "scripts-interval-sa@scripts-interval.iam.gserviceaccount.com",
+        "client_id": "115821091947389819402",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/scripts-interval-sa%40scripts-interval.iam.gserviceaccount.com",
+        "universe_domain": "googleapis.com",
     }
     gcreds = Credentials.from_service_account_info(credentials, scopes=scopes)
     client = gspread.authorize(gcreds)
     return client.open_by_key(SHEET_ID).worksheet(str(curr_year))
+
+
+def get_target_group(request_session: Session):
+    groups = request_session.get(
+        f"{BASE_URL}/userGroups/{user_id}.json",
+    )
+    groups = groups.json()
+
+    for group_id, metadata in groups.items():
+        group = request_session.get(f"{BASE_URL}/groups/{group_id}.json")
+        group = group.json()
+        group_name = group.get("name")
+        if group_name == f"{CURR_MONTH} 2025":
+            return group_id
+
+
+def post_create_transaction(request_session: Session, group_id: str):
+    import time
+
+    ms = time.time_ns() // 1_000_000
+    payload = {
+        "category": "😃",
+        "currencyCode": "PHP",
+        "dateTime": ms,
+        "exchangeRates": {"PHP": "1"},
+        "fixedExchangeRate": False,
+        "items": [
+            {
+                "amount": "500",
+                "forWhom": [{"memberId": "-OI-8lqJ49B4yX7lpjCP", "weight": "1"}],
+            }
+        ],
+        "purpose": "TEST API",
+        "type": "expense",
+        "whoPaid": [{"memberId": "-OI-96V4JWwoxjP2mUzQ", "weight": "1"}],
+    }
+    response = request_session.post(
+        f"{BASE_URL}/transactions/{group_id}.json", json=payload
+    )
+    response = response.json()
+
+    return response
+
+
+def compute_transaction_total(request_session: Session, group_id: str):
+    if not group_id:
+        logger.error("Target group id is not found")
+        return
+
+    transactions = request_session.get(f"{BASE_URL}/transactions/{group_id}.json")
+    transactions = transactions.json()
+    total = 0
+
+    for key, value in transactions.items():
+        items = value.get("items", [])
+
+        for item in items:
+            total += float(item.get("amount", 0))
+
+    return total
+
 
 if __name__ == "__main__":
     CURR_MONTH = datetime.now(tz=timezone.utc).strftime("%B")
@@ -54,7 +115,7 @@ if __name__ == "__main__":
         "authDomain": DOMAIN,
         "databaseURL": f"https://{DOMAIN}",
         "storageBucket": f"{NAMESPACE}.appspot.com",
-        "projectId": NAMESPACE
+        "projectId": NAMESPACE,
     }
 
     firebase = pyrebase.initialize_app(config)
@@ -64,34 +125,11 @@ if __name__ == "__main__":
     BASE_URL = f"https://{DOMAIN}"
 
     with requests.Session() as session:
-        session.params = {
-            "auth": creds.get("idToken")
-        }
-        groups = session.get(
-            f"{BASE_URL}/userGroups/{user_id}.json",
-        )
-        groups = groups.json()
+        session.params = {"auth": creds.get("idToken")}
+        target_group_id = get_target_group(session)
+        total = compute_transaction_total(session, target_group_id)
 
-        target_group_id = None
-        for group_id, metadata in groups.items():
-            group = session.get(f"{BASE_URL}/groups/{group_id}.json")
-            group = group.json()
-            group_name = group.get("name")
-            if group_name == f"{CURR_MONTH} 2025":
-                target_group_id = group_id
-                break
-
-        if target_group_id:
-            transactions = session.get(f"{BASE_URL}/transactions/{target_group_id}.json")
-            transactions = transactions.json()
-            total = 0
-
-            for key, value in transactions.items():
-                items = value.get("items", [])
-
-                for item in items:
-                    total += float(item.get("amount", 0))
-
+        if total is not None:
             sheet = init_gspread()
             row_cell = sheet.find("Food")
             col_cell = sheet.find(month_name)
