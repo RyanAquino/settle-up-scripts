36e9bd5b1332739709fb73b893c39d167bd4fd0d
RyanAquino <ryan.aquino7110@gmail.com>
2025-06-14 23:53:26 +0900

    logger

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 from requests import Session
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
 logger.info(f"Loading envs... : {load_dotenv(verbose=True)}")
 
 NAMESPACE = os.getenv("NAMESPACE")
